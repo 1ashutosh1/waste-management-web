2a8822c603e3d51df2ce0d94f8a29a1c6f570a6d
1ashutosh1 <at8969927@gmail.com>
2024-10-23 23:51:26 +0530

    db setup bug fixed

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,11 +1,11 @@
+import { url } from "inspector";
 export default {
     dialect: "postgresql",
     schema: "./src/utils/db/schema.ts",
     out: "./drizzle",
     dbCredentials: {
-      url: "postgresql://zerotohero_owner:SPVe2rokvBf6@ep-bitter-shadow-a5lbwa60.us-east-2.aws.neon.tech/zerotohero?sslmode=require",
-      connectionString:
-        "postgresql://zerotohero_owner:SPVe2rokvBf6@ep-bitter-shadow-a5lbwa60.us-east-2.aws.neon.tech/zerotohero?sslmode=require",
+      url: process.env.NEXT_PUBLIC_DATABASE_URL,
+      connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
     },
   };
   
--- src/utils/db/dbConfig.jsx
+++ src/utils/db/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://zerotohero_owner:SPVe2rokvBf6@ep-bitter-shadow-a5lbwa60.us-east-2.aws.neon.tech/zerotohero?sslmode=require"
+  process.env.NEXT_PUBLIC_DATABASE_URL
 );
 export const db = drizzle(sql, { schema });
